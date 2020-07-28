91244c2fe73aba8c871f2e264b63faac4b9b550f
David Pine <david.pine@microsoft.com>
2020-07-28 15:27:07 -0500

    Fixed formatting and got dynamic python discovering working

--- src/nbconvert.ts
+++ src/nbconvert.ts
@@ -1,4 +1,5 @@
-import { spawn, execSync, ExecException } from 'child_process';
+import * as os from 'os';
+import { spawn, execSync } from 'child_process';
 import { resolve, join } from 'path';
 
 export interface IResult {
@@ -18,9 +19,13 @@ export function convertToMarkdown(
     ];
 
     let result: IResult | null = null;
-    const stdout = execSync('which python').toString();
-    const pythonPath = stdout.substr(0, stdout.length - 1);
-    const jupyterPath = join(pythonPath, 'scripts/jupiter');
+    const plat = os.platform();
+    const stdout = execSync(`${(plat === 'win32' ? 'where' : 'which')} python`).toString();
+
+    let pythonPath = stdout.split('\r\n')[0].substr(0, stdout.length - 1);
+    pythonPath = pythonPath.replace('python.exe', '');
+
+    const jupyterPath = join(pythonPath, 'Scripts/jupyter');
 
     const path = resolve(jupyterPath);
     let nbConvert = spawn(path, args, { windowsVerbatimArguments: true });
@@ -34,7 +39,10 @@ export function convertToMarkdown(
 
     nbConvert.on('exit', (code, _) => {
         if (code !== 0) {
-           callback(null, { name: "UnableToConvertError", message: `Exited with error code: ${code}.` });
+            callback(null, {
+                name: "FileSystemError",
+                message: `Unable to convert "${ipynbPath}" to markdown - error code: ${code}`
+            });
         } else {
             callback(result, null);
         }
