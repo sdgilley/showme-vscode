ea05275104cfe78197c9d43af1319c8fae4e2bff
David Pine <david.pine@microsoft.com>
2020-07-28 12:44:56 -0500

    Got it working

--- src/nbconvert.ts
+++ src/nbconvert.ts
@@ -8,36 +8,31 @@ export interface IResult {
 export function convertToMarkdown(
     ipynbPath: string,
     callback: (result: IResult | null, errorOrNull: Error | null) => void) {
-    //exec('which jupyter nbconvert', (err: ExecException | null, stdout: string, stderr: string) => {
-    // if (err) {
-    //     callback(null, err);
-    // }
 
-    // if (!stdout) {
-    //     throw new Error('Cannot find nbconvert - is it installed?');
-    // }
-
-    //const nbConvertPath = stdout.substr(0, stdout.length - 1);
-    const args = [];
-    args.push('nbconvert', '--to', 'markdown', `"${ipynbPath}"`, '--stdout');
+    const args: string[] = [
+        'nbconvert',
+        '--to',
+        'markdown',
+        `"${ipynbPath}"`,
+        '--stdout'
+    ];
 
     let result: IResult | null = null;
     const path = resolve('C:\\Users\\dapine\\AppData\\Local\\Programs\\Python\\Python38-32\\Scripts\\jupyter');
-    let nbConvert = spawn(path, args);
+    let nbConvert = spawn(path, args, { windowsVerbatimArguments: true });
     nbConvert.stdout.on('data', data => {
         result = { markdown: data + '' };
     });
 
-    nbConvert.on('error', (err) => {
+    nbConvert.on('error', err => {
         callback(null, err);
     });
 
     nbConvert.on('exit', (code, _) => {
         if (code !== 0) {
-            callback(null, { name: "UnableToConvertError", message: `Exited with error code: ${code}.` });
+           callback(null, { name: "UnableToConvertError", message: `Exited with error code: ${code}.` });
         } else {
             callback(result, null);
         }
     });
-    //});
 }
